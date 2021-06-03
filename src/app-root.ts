1c2d88545929fb65ac5b64546f72230dcc977782
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-06-03 22:24:28 +0200

    chore: add simple display

--- src/app-root.ts
+++ src/app-root.ts
@@ -3,6 +3,7 @@ import { ViewController } from "viewer/controller";
 
 export class AppRoot extends HTMLElement {
     viewController: ViewController;
+    data:{keys:any[], values:any[]} = {keys:[], values:[]}
 
     public connectedCallback() {
         this.render();
@@ -12,10 +13,41 @@ export class AppRoot extends HTMLElement {
 
     handleEvent(e:any){
         console.log(e)
+        if(e?.data){
+            const keys = Object.keys(e.data)
+            const values = keys.map((x)=>{
+                if(e.data[x]?.value){
+                    return e.data[x]?.value
+                } else{
+                    return e.data[x]
+                }
+            })
+
+            keys.unshift('element type')
+            values.unshift(e.data?.constructor?.name)
+
+            this.data = {keys, values}
+            this.render()
+        }
     }
 
     public render(){
         render(this.template(), this);
+    }
+
+    private getData(){
+        const {values, keys} = this.data;
+
+                        if(keys.length){
+                            return html`
+                            <div class="text-center bg-indigo-500">Data:</div>
+                            ${keys.map((key, i)=>{
+                                return html`<div><span class="font-semibold">${key}:</span><span>${values[i]}</span></div>`
+                            })}
+                        `
+                        } else{
+                            return 'no data'
+                        }
     }
 
     public template() {
@@ -23,13 +55,18 @@ export class AppRoot extends HTMLElement {
             <!--   template -->
             <canvas class="top-0 absolute w-full h-full" id="3dview"> </canvas>
             <label class="inline-block p-2 m-2 bg-indigo-300 z-10 relative">
-                <input class="hidden" @change=${async (e: any) => {
+                <input type = "file" class="hidden" @change=${async (e: any) => {
                         this.viewController.readFile(e.target.files[0])
                 }}
-                type = "file"
-                value=""
                 />
                 open file
-                </label>`;
+                </label>
+
+
+                <div class="bottom-0 right-0 absolute bg-indigo-300 m-2 p-2 flex flex-col">
+                    
+                    ${this.getData()}
+                </div>
+                `;
     }
 }
