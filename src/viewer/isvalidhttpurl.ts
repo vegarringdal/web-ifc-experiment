47ea7c0ae54f333c6387a146a48ab408a0e85830
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-06-15 19:35:39 +0200

    fix: isvalidURL - check if value is string before using it

--- src/viewer/isvalidhttpurl.ts
+++ src/viewer/isvalidhttpurl.ts
@@ -1,4 +1,8 @@
 export function isValidHttpUrl(string: string) {
+    if (typeof string !== "string") {
+        return false;
+    }
+
     if (string.includes("https://") || string.includes("http://")) {
         return true;
     } else {
